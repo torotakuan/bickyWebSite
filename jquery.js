c85e822529c1e349cfd66c713038c8c55e74bdce
fujimon0211 <89776152+fujimon0211@users.noreply.github.com>
2021-09-06 21:31:23 +0900

    Update jquery.js

--- jquery.js
+++ jquery.js
@@ -7,30 +7,18 @@ $(function(){
 
 });
 
-axios.get("instagram.php").then(instagram_data=>{
-  console.log(instagram_data);// 検証ツールのConsoleを覗くと取得したデータの全容が確認できます。
-    
-  //
-  //他のInstagramビジネスアカウントの投稿情報も取得したい場合
-  
-  const gallery_data = instagram_data["data"]["business_discovery"]["media"]["data"];
+$.getJSON("php/instagram.php", function(instagram_data){
 
-  //
-  //自分のInstagramビジネスアカウントの投稿情報が取得できればOKな場合は
-  //const gallery_dataを下記にする。
+const gallery_data = instagram_data["business_discovery"]["media"]["data"];
+let photos = "";
+const photo_length = 9;
 
-  // const gallery_data = instagram_data["data"]["media"]["data"];
+for(let i = 0; i < photo_length ;i++){
+photos += '<li class="gallery-item"><img src="' + gallery_data[i].media_url + '"></li>';
+}
+$("#gallery").append(photos);
 
-  let photos = "";
-  const photo_length = 9;
-
-  for(let i = 0; i < photo_length ;i++){
-    photos += '<li class="gallery-item"><img src="' + gallery_data[i].media_url + '"></li>';
-  } 
-  document.querySelector("#gallery").innerHTML = photos;
-}).catch(error=>{
-  console.log(error);
-})
+});
 
 $(function(){
   setTimeout('stopload()',3000);
