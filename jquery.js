abf864ad97c450b4ba5440b498daab7910991359
fujimon0211 <89776152+fujimon0211@users.noreply.github.com>
2021-09-06 21:46:57 +0900

    Update jquery.js

--- jquery.js
+++ jquery.js
@@ -38,7 +38,7 @@ $(function(){
     var data;
     var graph_api = 'https://graph.facebook.com/v7.0/me';
     var param = '?fields=posts{message, full_picture}';
-    var token = 'トークンをここに入力';
+    var token = EAAVeXxsR9D8BACMLIJqnab3hK9NUawN3IPYToCmdQxlAVeVoDkFnFf3JcvAgRrC5ug7UYdnZA7UBbYGaMQs5eI5pNqcckbp5ZA8vZAQMXJAsl60bRB2LdQ6ZBcP7wqDwzR7Eku1ZBr0b9b2gjQUEJAGX9XutW1jSx98H8qwAc9Tzw9ZAuBmDT75pg5H3qL4nqR6qZBxSA4NvQZDZD;
     $.ajax({
         url: graph_api + param + '&access_token=' + token
       })
