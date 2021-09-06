1e7e69531cff6c65cee4101309ecfda436a54c32
torotakuan <41503244+torotakuan@users.noreply.github.com>
2021-09-06 12:58:58 +0900

    Update jquery.js

--- jquery.js
+++ jquery.js
@@ -7,6 +7,29 @@ $(function(){
 
 });
 
+$.ajax({
+type: 'GET',
+url: 'https://graph.facebook.com/v8.0/【InstagramビジネスアカウントID】?fields=name%2Cmedia.limit(【表示したい画像数】)%7Bcaption%2Clike_count%2Cmedia_url%2Cpermalink%2Cthumbnail_url%7D&access_token=【アクセストークン】',
+dataType: 'json',
+ success: function(json) {
+var ig = json.media.data;
+var html = "";
+for (var i = 0; i < ig.length; i++) {
+var link = ig[i].permalink;
+var image
+if(!ig[i].thumbnail_url) {
+// 動画の場合はこちら
+image = ig[i].media_url;}
+else {
+// 写真の場合はこちら
+image = ig[i].thumbnail_url;
+}
+html += '<div><a href="' + link + '" target="_blank"><img src="' + image + '"></a></div>'
+}
+$(".instagram").append(html);
+}
+});
+
 $(function(){
   setTimeout('stopload()',3000);
 });
