316a54d57b4cd8f2fa5f9b23b95974b15f73d458
torotakuan <41503244+torotakuan@users.noreply.github.com>
2021-09-08 14:44:32 +0900

    yamablogのコピペ

--- photo.js
+++ photo.js
@@ -1,47 +1,26 @@
-function DomEdit(_fbObj) {
-    var elmText = document.querySelector('#app-text') || false
-    var elmImg = document.querySelector('#app-img') || false
-    var noImgSrc = 'https://www.rakumachi.jp/images/frontend/smartphone/nophoto_thumbnail.png'
-    if (_fbObj) {
-      if (elmText) {
-        var text = _fbObj.posts.data[0].message || false
-        if (text) {
-          elmText.textContent = text
-        } else {
-          elmText.textContent = '本文が無いです。'
-        }
-      }
-      if (elmImg) {
-        var src = _fbObj.posts.data[0].full_picture || false
-        if (src) {
-          elmImg.setAttribute('src', src)
-        } else {
-          elmImg.setAttribute('src', noImgSrc)
-        }
-      }
-    }
-  }
 
-  // AjaxとAPIで情報を取得
-  function GetFacebook() {
-    var data;
-    var graph_api = 'https://graph.facebook.com/v7.0/me';
-    var param = '?fields=posts{message, full_picture}';
-    var token = 'EAAVeXxsR9D8BACMLIJqnab3hK9NUawN3IPYToCmdQxlAVeVoDkFnFf3JcvAgRrC5ug7UYdnZA7UBbYGaMQs5eI5pNqcckbp5ZA8vZAQMXJAsl60bRB2LdQ6ZBcP7wqDwzR7Eku1ZBr0b9b2gjQUEJAGX9XutW1jSx98H8qwAc9Tzw9ZAuBmDT75pg5H3qL4nqR6qZBxSA4NvQZDZD';
-    $.ajax({
-        url: graph_api + param + '&access_token=' + token
-      })
-      .done(function (res) {
-        data = res.data;
-      })
-      .fail(function (jqXHR, status) {
-        console.log(status)
-      })
-      .always(function (res) {
-        console.log(res)
-        DomEdit(res)
-      });
+
+
+
+$.ajax({
+  type: 'GET',
+  url: 'https://graph.facebook.com/v8.0/1511137452618815?fields=name%2Cmedia.limit(1)%7Bcaption%2Clike_count%2Cmedia_url%2Cpermalink%2Cthumbnail_url%7D&access_token=EAAVeXxsR9D8BACMLIJqnab3hK9NUawN3IPYToCmdQxlAVeVoDkFnFf3JcvAgRrC5ug7UYdnZA7UBbYGaMQs5eI5pNqcckbp5ZA8vZAQMXJAsl60bRB2LdQ6ZBcP7wqDwzR7Eku1ZBr0b9b2gjQUEJAGX9XutW1jSx98H8qwAc9Tzw9ZAuBmDT75pg5H3qL4nqR6qZBxSA4NvQZDZD',
+  dataType: 'json',
+   success: function(json) {
+  var ig = json.media.data;
+  var html = "";
+  for (var i = 0; i < ig.length; i++) {
+  var link = ig[i].permalink;
+  var image
+  if(!ig[i].thumbnail_url) {
+  // 動画の場合はこちら
+  image = ig[i].media_url;}
+  else {
+  // 写真の場合はこちら
+  image = ig[i].thumbnail_url;
   }
-  window.addEventListener('load', () => {
-    GetFacebook()
-  }) 
+  html += '<div><a href="' + link + '" target="_blank"><img src="' + image + '"></a></div>'
+  }
+  $(".instagram").append(html);
+  }
+});
