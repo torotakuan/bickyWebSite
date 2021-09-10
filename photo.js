c8170f5e0b8cfebfec2555eb2c7e8853978daf0e
torotakuan <41503244+torotakuan@users.noreply.github.com>
2021-09-10 16:15:47 +0900

    id token change

--- photo.js
+++ photo.js
@@ -1,6 +1,6 @@
 $.ajax({
   type: 'GET',
-  url: 'https://graph.facebook.com/v8.0/203267225056267?fields=name%2Cmedia.limit(1)%7Bcaption%2Clike_count%2Cmedia_url%2Cpermalink%2Cthumbnail_url%7D&access_token=EAAVeXxsR9D8BACMLIJqnab3hK9NUawN3IPYToCmdQxlAVeVoDkFnFf3JcvAgRrC5ug7UYdnZA7UBbYGaMQs5eI5pNqcckbp5ZA8vZAQMXJAsl60bRB2LdQ6ZBcP7wqDwzR7Eku1ZBr0b9b2gjQUEJAGX9XutW1jSx98H8qwAc9Tzw9ZAuBmDT75pg5H3qL4nqR6qZBxSA4NvQZDZD',
+  url: 'https://graph.facebook.com/v9.0/17841434273658865?fields=name%2Cmedia.limit(1)%7Bcaption%2Clike_count%2Cmedia_url%2Cpermalink%2Ctimestamp%2Cusername%7D&access_token=EAAVeXxsR9D8BAIeOs6mfE3YXyZBRFNikEmwau4p1zSzTShrwZBVXwwyBszpvh3EqEtT5EyRjZBNZBjwX0FPbTZA9HqXsqIiXfYyXOdVdhX3U2ZBdC1MxzTUs4RQRoGVaKWiYOZBLKxVFdbvgz7dcJEWUwMWIFGECXo03vJTt7aPvYd8YlgybEoUNFJv7fG9IZCFmXSQO1KxPHgZDZD',
   dataType: 'json',
    success: function(json) {
   var ig = json.media.data;
