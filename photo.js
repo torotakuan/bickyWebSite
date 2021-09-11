ba1e79564ca4206a51370cb3b844cd4d517a277c
fujimon0211 <89776152+fujimon0211@users.noreply.github.com>
2021-09-11 16:19:07 +0900

    Update photo.js

--- photo.js
+++ photo.js
@@ -1,47 +1,32 @@
-function DomEdit(_fbObj) {
-    var elmText = document.querySelector('#app-text') || false
-    var elmImg = document.querySelector('#app-img') || false
-    var noImgSrc = 'https://www.rakumachi.jp/images/frontend/smartphone/nophoto_thumbnail.png'
-    if (_fbObj) {
-      if (elmText) {
-        var text = _fbObj.posts.data[0].message || false
-        if (text) {
-          elmText.textContent = text
-        } else {
-          elmText.textContent = '本文が無いです。'
+FB.api(
+  '/17841434273658865/media',
+  'GET',
+  {"fields":"id,media_url,thumbnail_url,permalink"},
+  function(response) {
+      // Insert your code here
+      $.ajax({
+        type: 'GET',
+        url: 'https://graph.facebook.com/v8.0/17841434273658865?fields=name%2Cmedia.limit(5)%7Bcaption%2Clike_count%2Cmedia_url%2Cpermalink%2Cthumbnail_url%7D&access_token=EAAVeXxsR9D8BAOj6c1ZCTv5pD9XQXUvhGALrM8RRcQh892fEk54PougvZBtQk1ad2qnbrdnO67CTkxFSd6BZAign0m2jgprvGwmZCIGhpQv0YZCyw9QNVaHuUhVEFvphwTfZC8rK6QihbHTqi1RFSfWOYZBnsW8s5bfTEWjtZCazY5w66ri2jq79',
+        dataType: 'json',
+         success: function(json) {
+        var ig = json.media.data;
+        var html = "";
+        for (var i = 0; i < ig.length; i++) {
+        var link = ig[i].permalink;
+        var image
+        if(!ig[i].thumbnail_url) {
+        // 動画の場合はこちら
+        image = ig[i].media_url;}
+        else {
+        // 写真の場合はこちら
+        image = ig[i].thumbnail_url;
         }
-      }
-      if (elmImg) {
-        var src = _fbObj.posts.data[0].full_picture || false
-        if (src) {
-          elmImg.setAttribute('src', src)
-        } else {
-          elmImg.setAttribute('src', noImgSrc)
+        html += '<div><a href="' + link + '" target="_blank"><img src="' + image + '"></a></div>'
         }
-      }
-    }
+        $(".instagram").append(html);
+        }
+        });
   }
+);
 
-  // AjaxとAPIで情報を取得
-  function GetFacebook() {
-    var data;
-    var graph_api = 'https://graph.facebook.com/v7.0/me';
-    var param = '?fields=posts{message, full_picture}';
-    var token = 'EAAVeXxsR9D8BACMLIJqnab3hK9NUawN3IPYToCmdQxlAVeVoDkFnFf3JcvAgRrC5ug7UYdnZA7UBbYGaMQs5eI5pNqcckbp5ZA8vZAQMXJAsl60bRB2LdQ6ZBcP7wqDwzR7Eku1ZBr0b9b2gjQUEJAGX9XutW1jSx98H8qwAc9Tzw9ZAuBmDT75pg5H3qL4nqR6qZBxSA4NvQZDZD';
-    $.ajax({
-        url: graph_api + param + '&access_token=' + token
-      })
-      .done(function (res) {
-        data = res.data;
-      })
-      .fail(function (jqXHR, status) {
-        console.log(status)
-      })
-      .always(function (res) {
-        console.log(res)
-        DomEdit(res)
-      });
-  }
-  window.addEventListener('load', () => {
-    GetFacebook()
-  }) 
+ 
