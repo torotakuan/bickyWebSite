76d3d96b5bc11aa00e4df9b0150a27a2e7e67965
torotakuan <41503244+torotakuan@users.noreply.github.com>
2022-03-01 16:36:50 +0900

    エラーメッセージを表示する機能の追加

--- contact.js
+++ contact.js
@@ -2,17 +2,38 @@ const error = {
     empty : false,//空欄の有る無し
     email : false//メールが使用可能なものか
 };
-window.contact = window.contact || {};
+var afterSubmit = false;//送信ボタンを押した後か前か
+window.contact = window.contact || {};//グローバルのオブジェクトとしてcontactを宣言し、contactの中身が空の場合は中身を空のオブジェクトにしておく
 window.contact.checkValidation = function(){
-    //valの中身がnull、空白の場合実行（.valの中身がないとfalse判定になるため、!をつけて、.valの中身がない場合にtrue判定にさせる）
+    //.valの中身に１つでもnull、空白がある場合    （if文の中身: .valの中身がないとfalse判定になる仕様のため、!をつけて、.valの中身がない場合にtrue判定にさせる）
     if(!$('input[id="name"]').val() || !$('input[id="email"]').val() || !$('input[id="age"]').val() ||!$('input[id="male"]').val() ||!$('input[id="female"]').val() ||!$('select[id="conduct-contents"]').val() ||!$('textarea[id="message"]').val()){
         $('input[id=submit]').attr('disabled', 'disabled');
         error.empty = true;
-        return false;
+        if(afterSubmit){
+            $(".empty-error").html("未記入の項目があります");
+        }
     }else{
+        $(".empty-error").html("");
         error.empty = false;
     }
-    
+
+
+    const regex = /^[a-zA-Z0-9_+-]+(\.[a-zA-Z0-9_+-]+)*@([a-zA-Z0-9][a-zA-Z0-9-]*[a-zA-Z0-9]*\.)+[a-zA-Z]{2,}$/;//使用可能なメールアドレスかの条件
+    const email = $('input[id="email"]').val();
+    if(email){//emailに入力がある場合
+        if (!regex.test(email)){//使用可能なメールアドレスかチェック
+            error.email = true;
+            $(".email-error").html("使用可能なメールアドレスを入力してください");
+        }else{
+            error.email = false;
+            $(".email-error").html("");
+
+        }
+    }
+
+    if(error.empty || error.email){//エラーが１つでもあるならばfalseを返してここより下の処理は行わない
+        return false;
+    }
 
     $('input[id="submit"]').removeAttr('disabled');
     return true;
