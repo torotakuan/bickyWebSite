c3f55321529bce0314a5d7c627fc61fffc1605f9
torotakuan <41503244+torotakuan@users.noreply.github.com>
2022-03-18 15:31:23 +0900

    newsページにツイッタータイムライン埋め込み

--- news.js
+++ news.js
@@ -1,15 +1,58 @@
-   $(function()}
-    $("#side-menu-btn").click(function(){
 
-        $(".side-menu").fadeIn();
-      
+window.onload = function(){
+console.log("onloaded");
+console.log($(".timeline-Header").css("padding"));
+}
+$(function(){
+  document.body.appendChild(
+      $("<script>").prop(
+          {
+              src: "https://platform.twitter.com/widgets.js"
+          }
+      )[0]
+  );
+  console.log($(".timeline-Header").css("padding"));
+  $(".timeline-Header").css("display" ,"none");
   
-      });
+  //こっからメニューボタン関連--------------------------------------------------------------------------------------
+  var open = false;
+  $(".openbtn").click(function () {
+    if(!open){
+      $(this).toggleClass('active');
+      $(".side-menu").fadeIn();
+      open = true;
+    }else{
+      $(this).toggleClass('active');
+      $(".side-menu").fadeOut();
+      open = false;
+
+    }
+    
+  });
+  //ここまでメニューボタン関連--------------------------------------------------------------------------------------
+
+  //こっからパスワード関連--------------------------------------------------------------------------------------
+  var pw;
+  var keys = new Object();
+
+  //cookieから、パスワードが入力されたかの情報を取り出す
+  var elements = document.cookie.split(";");
+  for ( var element of elements){
+    var val = element.split("=");
+    keys[val[0]] = val[1];
+  }
+
+  if(keys[" key"] == "open"){
+    $(".filter").fadeOut();
+  }else{
+    pw = prompt("このページは工事中です パスワードを入れて下さい"  ,"");
+    if (pw == "bicky"){
+      document.cookie = "key=open";
+      $(".filter").fadeOut();
+    }else{
+      alert("パスワードが違います！");
+    }
+  }
+  //ここまでパスワード関連--------------------------------------------------------------------------------------
   
-      $(".side-menu-top").click(function(){
-  
-        $(".side-menu").fadeOut();
-  
-  
-      });
-      });
+});
