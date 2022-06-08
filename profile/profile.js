d57dd8e6764faf4cba7a87fd96714378ec8fea94
torotakuan <41503244+torotakuan@users.noreply.github.com>
2022-06-08 19:42:24 +0900

    プロフィールの一部修正し忘れていたパスの変更と最初から全文表示に切り替え

--- profile/profile.js
+++ profile/profile.js
@@ -1,18 +1,18 @@
 $(function () {
 
 
-    $(".detail-open-btn").click(function () {
-        $(".detail-open-btn").hide()
-        $(".detail-section").fadeIn(2000)
-        $(".main-sentence").hide()
+    // $(".detail-open-btn").click(function () {
+    //     $(".detail-open-btn").hide()
+    //     $(".detail-section").fadeIn(2000)
+    //     $(".main-sentence").hide()
 
-    })
-    $(".detail-close-btn").click(function () {
-        $(".detail-section").hide()
-        $(".detail-open-btn").show()
-        $(".main-sentence").show()
+    // })
+    // $(".detail-close-btn").click(function () {
+    //     $(".detail-section").hide()
+    //     $(".detail-open-btn").show()
+    //     $(".main-sentence").show()
    
-    })
+    // })
     $(".openbtn").click(function () {
         if ($('.side-menu').css('display') == 'none') {
             // 表示されている場合の処理
